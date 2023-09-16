4e51680c151aad11224f794708ebf368cb4c4195
pawan birnale <birnalepavan@gmail.com>
2023-09-16 20:48:04 +0530

    registration form get data in front end

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,23 +3,18 @@ import pawan from '../images/photo.png'
 const About = () => {
   return (
     <>
-      <div className="about_page">
-        <div className="about_container">
-          <div className="row-1 d-flex">
+      <div className="about_container">
+        <div className="about_child">
+          <div className="row-1 d-flex justify-content-around">
             <div className="photo">
-              <figure>
-                <img src={pawan} className='' alt="" />
-              </figure>
+              <img src={pawan} alt="photo" />
             </div>
-            <div className="intro text-center mt-5 ">
-              <h3>Pawan Birnale</h3>
-              <p>Web Developer</p>
-            </div>
+            <div className="biodata"></div>
           </div>
-          <div className="row-2 d-flex justify-content-evenly mt-3">
-            <div className="about_row-2_contain ">1</div>
-            <div className="about_row-2_contain">2</div>
-            <div className="about_row-2_contain">3</div>
+          <div className="row-2">
+            <div className="col">1</div>
+            <div className="col">2</div>
+            <div className="col">3</div>
           </div>
         </div>
       </div>
